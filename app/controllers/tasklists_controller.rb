7118dce539941683e40f7c4476d829bb02c0b4f2
kosuke.umehara <umeekko@gmail.com>
2020-03-31 08:33:54 +0000

    add pagination

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -2,7 +2,7 @@ class TasklistsController < ApplicationController
   before_action :set_tasklist, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasklists = Tasklist.all
+      @tasklists = Tasklist.order(id: :desc).page(params[:page]).per(3)
   end
 
   def show
